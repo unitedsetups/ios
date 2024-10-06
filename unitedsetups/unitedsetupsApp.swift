3016f65b1cc29898d8fc7aafe8db88b9ef1868d9
Paras Khanchandani <dev@paraskcd.com>
2024-10-06 11:05:15 +0200

    Initial Commit

---
+++ unitedsetups/unitedsetupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  unitedsetupsApp.swift
+//  unitedsetups
+//
+//  Created by Paras KCD on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct unitedsetupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
